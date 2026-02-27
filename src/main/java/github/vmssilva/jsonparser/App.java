a2b9198f4cbbdeca0ae5335125675d7d7140bd37
dida <vsilva.ad@gmail.com>
2026-02-27 15:19:32 -0300

    Remove unnecessary code used for debugging

--- src/main/java/github/vmssilva/jsonparser/App.java
+++ src/main/java/github/vmssilva/jsonparser/App.java
@@ -1,21 +1,6 @@
 package github.vmssilva.jsonparser;
 
-import github.vmssilva.jsonparser.lexer.Lexer;
-
 public class App {
   public static void main(String[] args) {
-
-    var source = """
-        {
-            "name": "\\\"John\\\\n\"
-        }
-          """;
-    source = "{ \"symbol\": \"\u0041\" }";
-    var lexer = new Lexer(source);
-
-    var tk = lexer.tokenize();
-
-    while (tk.hasMoreTokens())
-      System.out.println(tk.nextToken());
   }
 }
